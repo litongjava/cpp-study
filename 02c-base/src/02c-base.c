f935647575adfd4eac9f0cae4e5f484d8e58cde8
litngjava <litongjava@qq.com>
2019-10-24 19:57:07 +0800

    php72w-common-7.2.22-1.w7.x86_64

--- 02c-base/src/02c-base.c
+++ 02c-base/src/02c-base.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(void) {
-  char src[50], dest[50];
-  strcpy(src, "this is source");
-  strcpy(dest, "this is dest");
-  strcat(dest, src);
-  printf("×îÖÕµÄ×Ö·û´®ÊÇ:%s", dest);
+  int b[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+  int size = sizeof(b);
+  printf("%d\n", size);
 }
+
